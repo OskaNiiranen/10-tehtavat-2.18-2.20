e5894321a6dcb0ce947603446dda2669b0c4ddd2
Niiranen Oska <oska.niiranen@gmail.com>
2023-12-21 13:56:59 +0200

    2.20

--- src/components/RenderOne.jsx
+++ src/components/RenderOne.jsx
@@ -1,5 +1,34 @@
+import { useState, useEffect } from "react";
+
 const RenderOne = ({ filteredCountries }) => {
+  const [weatherData, setWeatherData] = useState(null);
   const country = filteredCountries[0];
+  const api_key = import.meta.env.VITE_SOME_KEY;
+
+  useEffect(() => {
+    if (country.capital) {
+      fetch(
+        `https://api.openweathermap.org/data/2.5/weather?q=${country.capital}&appid=${api_key}&units=metric`
+      )
+        .then((response) => {
+          if (!response.ok) {
+            throw new Error(response.status);
+          }
+          return response.json();
+        })
+        .then((data) => setWeatherData(data))
+        .catch((error) => {
+          if (error.message === "401") {
+            alert(
+              "Unauthorized access. Please ensure your API key is correctly set in the .env file."
+            );
+          } else {
+            console.error("Fetch error:", error);
+          }
+        });
+    }
+  }, [country.capital]);
+
   return (
     <div>
       <h2>{country.name.common}</h2>
@@ -24,6 +53,17 @@ const RenderOne = ({ filteredCountries }) => {
           alt={`Flag of ${country.name.common}`}
           width="200"
         />
+      )}
+      <h3>Weather in {country.capital}</h3>
+      {weatherData && (
+        <div>
+          <p>Temperature: {weatherData.main.temp}°C</p>
+          <p>Wind: {weatherData.wind.speed} m/s</p>
+          <img
+            src={`http://openweathermap.org/img/w/${weatherData.weather[0].icon}.png`}
+            alt="Weather icon"
+          />
+        </div>
       )}
     </div>
   );
